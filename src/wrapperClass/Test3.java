56aff548eb37c15b2754ec8aa9eaa816749e2efb
Kaijar2020 <kizar.cse@gmail.com>
2024-04-02 19:33:48 +0600

    wrapper class

---
+++ src/wrapperClass/Test3.java
@@ -0,0 +1,19 @@
+package wrapperClass;
+
+
+import wrapperClass.pack1.Test1;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+//        t.m1();
+    }
+}
+/*
+ Access Modifier--
+
+ public -- access everywhere.
+ protected -- only through inheritance.
+ default -- only inside of package.
+ private -- only access within the class.
+*/
---
+++ src/wrapperClass/pack1/Test1.java
@@ -0,0 +1,9 @@
+package wrapperClass.pack1;
+
+public class Test1 {
+
+    protected int x = 100;
+    protected void m1(){
+        System.out.println("This is m1 from Test-1");
+    }
+}
